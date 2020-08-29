eabf3e796f127aa27d4ae18985bd5376d0a43b11
s2quake <han0210@netsgo.com>
2020-08-29 22:08:07 +0900

    서브모듈 업데이트 및 빌드 스크립트 추가

---
+++ build.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Released under the MIT License.
+
+REM Copyright (c) 2018 Ntreev Soft co., Ltd.
+REM Copyright (c) 2020 Jeesu Choi
+
+REM Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+REM documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+REM rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+REM persons to whom the Software is furnished to do so, subject to the following conditions:
+
+REM The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+REM Software.
+
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+REM WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+REM COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+REM OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+REM Forked from https://github.com/NtreevSoft/CommandLineParser
+REM Namespaces and files starting with "Ntreev" have been renamed to "JSSoft".
+
+powershell -executionpolicy remotesigned -File %~dp0\build.ps1 "%*"
---
+++ build.ps1
@@ -0,0 +1,38 @@
+# Released under the MIT License.
+
+# Copyright (c) 2018 Ntreev Soft co., Ltd.
+# Copyright (c) 2020 Jeesu Choi
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Forked from https://github.com/NtreevSoft/CommandLineParser
+# Namespaces and files starting with "Ntreev" have been renamed to "JSSoft".
+
+$location = Get-Location
+try {
+    Set-Location $PSScriptRoot
+    $buildFile = "./.vscode/build.ps1"
+    $propsPath = (
+        "../JSSoft.Library/Directory.Build.props",
+        "../JSSoft.Library.Commands/Directory.Build.props"
+    ) | ForEach-Object { "`"$_`"" }
+    $propsPath = $propsPath -join ","
+    $solutionPath = "./commands.sln"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile $buildFile
+    Invoke-Expression "$buildFile $solutionPath $propsPath $args"
+}
+finally {
+    Remove-Item ./.vscode/build.ps1
+    Set-Location $location
+}
