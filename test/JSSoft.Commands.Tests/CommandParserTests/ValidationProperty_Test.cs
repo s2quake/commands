b8d0fc74dd6b21f4fdd0579c04d5a89c12116b65
s2quake <han0210@netsgo.com>
2024-09-06 18:35:51 +0900

    test: Add test for validating data

--- test/JSSoft.Commands.Tests/CommandParserTests/ValidationProperty_Test.cs
+++ test/JSSoft.Commands.Tests/CommandParserTests/ValidationProperty_Test.cs
@@ -1,14 +1,14 @@
-// <copyright file="Validation_Test.cs" company="JSSoft">
+// <copyright file="ValidationProperty_Test.cs" company="JSSoft">
 //   Copyright (c) 2024 Jeesu Choi. All Rights Reserved.
 //   Licensed under the MIT License. See LICENSE.md in the project root for license information.
 // </copyright>
 
+#if !NETSTANDARD
 using System.ComponentModel.DataAnnotations;
 
 namespace JSSoft.Commands.Tests.CommandParserTests;
 
-#if !NETSTANDARD
-public class Validation_Test
+public class ValidationProperty_Test
 {
     private sealed class RegularExpressionClass
     {
@@ -38,6 +38,32 @@ public class Validation_Test
         Assert.Throws<CommandLineException>(() => parser.Parse(argumentLine));
     }
 
+    private static class RegularExpressionStaticClass
+    {
+        [CommandProperty]
+        [RegularExpression(@"^\d+")]
+        public static string Member1 { get; set; } = string.Empty;
+    }
+
+    [Fact]
+    public void RegularExpressionStaticClass_Member1_Test()
+    {
+        var parser = new CommandParser(typeof(RegularExpressionStaticClass));
+        var argumentLine = $"--member1 123";
+        parser.Parse(argumentLine);
+        var actualValue = RegularExpressionStaticClass.Member1;
+
+        Assert.Equal("123", actualValue);
+    }
+
+    [Fact]
+    public void RegularExpressionStaticClass_Member1_FailTest()
+    {
+        var parser = new CommandParser(typeof(RegularExpressionStaticClass));
+        var argumentLine = $"--member1 test";
+        Assert.Throws<CommandLineException>(() => parser.Parse(argumentLine));
+    }
+
     private sealed class MinLengthClass
     {
         [CommandProperty]
@@ -62,6 +88,32 @@ public class Validation_Test
     {
         var obj = new MinLengthClass();
         var parser = new CommandParser(obj);
+        var argumentLine = $"--member1 1";
+        Assert.Throws<CommandLineException>(() => parser.Parse(argumentLine));
+    }
+
+    private static class MinLengthStaticClass
+    {
+        [CommandProperty]
+        [MinLength(2)]
+        public static string Member1 { get; set; } = string.Empty;
+    }
+
+    [Fact]
+    public void MinLengthStaticClass_Member1_Test()
+    {
+        var parser = new CommandParser(typeof(MinLengthStaticClass));
+        var argumentLine = $"--member1 12";
+        parser.Parse(argumentLine);
+        var actualValue = MinLengthStaticClass.Member1;
+
+        Assert.Equal("12", actualValue);
+    }
+
+    [Fact]
+    public void MinLengthStaticClass_Member1_FailTest()
+    {
+        var parser = new CommandParser(typeof(MinLengthStaticClass));
         var argumentLine = $"--member1 1";
         Assert.Throws<CommandLineException>(() => parser.Parse(argumentLine));
     }
