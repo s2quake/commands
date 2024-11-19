e621e5ed6de4b8e93dedb295ac51b18ae9f6a9c0
s2quake <han0210@netsgo.com>
2024-11-19 21:31:06 +0900

    refactor: Remove CommandCompletionAttribute

--- src/JSSoft.Commands/CommandCompletionAttribute.cs
+++ src/JSSoft.Commands/CommandCompletionAttribute.cs
@@ -1,29 +1,29 @@
-// <copyright file="CommandCompletionAttribute.cs" company="JSSoft">
-//   Copyright (c) 2024 Jeesu Choi. All Rights Reserved.
-//   Licensed under the MIT License. See LICENSE.md in the project root for license information.
-// </copyright>
+// // <copyright file="CommandCompletionAttribute.cs" company="JSSoft">
+// //   Copyright (c) 2024 Jeesu Choi. All Rights Reserved.
+// //   Licensed under the MIT License. See LICENSE.md in the project root for license information.
+// // </copyright>
 
-namespace JSSoft.Commands;
+// namespace JSSoft.Commands;
 
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
-public class CommandCompletionAttribute : CommandStaticTypeAttribute
-{
-    public CommandCompletionAttribute(string methodName)
-    {
-        MethodName = methodName;
-    }
+// [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
+// public class CommandCompletionAttribute : CommandStaticTypeAttribute
+// {
+//     public CommandCompletionAttribute(string methodName)
+//     {
+//         MethodName = methodName;
+//     }
 
-    public CommandCompletionAttribute(string staticTypeName, string methodName)
-        : base(staticTypeName)
-    {
-        MethodName = methodName;
-    }
+//     public CommandCompletionAttribute(string staticTypeName, string methodName)
+//         : base(staticTypeName)
+//     {
+//         MethodName = methodName;
+//     }
 
-    public CommandCompletionAttribute(Type staticType, string methodName)
-        : base(staticType)
-    {
-        MethodName = methodName;
-    }
+//     public CommandCompletionAttribute(Type staticType, string methodName)
+//         : base(staticType)
+//     {
+//         MethodName = methodName;
+//     }
 
-    public string MethodName { get; }
-}
+//     public string MethodName { get; }
+// }
